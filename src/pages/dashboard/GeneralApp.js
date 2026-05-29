211dd1dfcb72d415af356dd07fc359ed8d07cc50
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-29 22:37:37 +0530

    started with the work with the generalapp of the dashboard page

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {Suspense, lazy} from "react";
 
 const GeneralApp = () => {
 
   return (
     <>
-      App
+      <Suspense fallback="loading..">
+          
+      </Suspense>
     </>
   );
 };
