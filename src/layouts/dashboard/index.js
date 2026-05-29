56ecfee909b3585395ab9c1034ea3049e18f498a
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-29 22:48:03 +0530

    now be working on the dahboard layout

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
@@ -5,7 +6,9 @@ const DashboardLayout = () => {
 
   return (
     <>
-      Dashboard Layout
+     <Box>
+      
+     </Box>
       <Outlet />
     </>
   );
