c123615194eab70ac9f38381b51e1f36cd07fb60
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-30 18:22:32 +0530

    puttted the logo of the chatapp on the sidebar

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -2,6 +2,8 @@ import { Box, useTheme } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import Logo from "../../assets/Images/logo.ico"
+
 const DashboardLayout = () => {
 
   const theme = useTheme()
@@ -12,8 +14,10 @@ const DashboardLayout = () => {
     <>
      <Box sx={({backgroundColor: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height: "100vh", width:100})}>
       <Box sx={{
-        backgroundColor: theme.palette.primary.main, height: 64 , width: 64, borderRadius: 
-      }}>
+        backgroundColor: theme.palette.primary.main, height: 64 , width: 64, borderRadius: 1.5
+      }} >
+        <img src={Logo} alt="chat app logo">
+        </img>
 
       </Box>
 
