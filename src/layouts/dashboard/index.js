58769b40fea2639019c014df98245d764c48bf03
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-30 18:50:08 +0530

    fixed an issue from the sidebar component

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,9 +1,8 @@
 import { Box, Divider, IconButton, Stack, useTheme } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Nav_Buttons } from "../../data";
+import { Nav_Buttons, Nav_Setting } from "../../data";
 import Logo from "../../assets/Images/logo.ico"
-import { Gear } from "phosphor-react";
 import { useState } from "react";
 
 const DashboardLayout = () => {
@@ -16,7 +15,7 @@ const DashboardLayout = () => {
 
   return (
     <>
-     <Box p={2} sx={({backgroundColor: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height: "100vh", width:100})}>
+     <Box p={2} sx={{backgroundColor: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height: "100vh", width:100}}>
 
       <Stack direction="column" sx={{width:"100%"}} alignItems={"center"} spacing={3}> 
       <Box sx={{
@@ -31,27 +30,28 @@ const DashboardLayout = () => {
       </Box>
       <Stack sx={{width: "max-content"}} direction="column" alignItems="center" spacing={3}>
         {Nav_Buttons.map((el)=> 
-        
-        (
-          el.index === selected ?
-          <Box sx={{backgroundColor: theme.palette.primary.main, borderRadius: 1.5}}>
-            <IconButton sx={{width:"max-content", color:"white"}} key={el.index}>{el.icon}</IconButton>
-          </Box> :  <IconButton onClick={()=>{
-            setSelected(el.index)
-          }} sx={{width:"max-content", color:"black"}} key={el.index}>{el.icon}</IconButton>
-        ))}
-        <Divider sx={{width: "48px"}}>
-
-        </Divider>
-        {selected === 3 ? 
-         <Box sx={{backgroundColor: theme.palette.primary.main, borderRadius: 1.5}}> 
-          <IconButton>
-          <Gear></Gear>
-        </IconButton>
-         </Box> : <IconButton>
-          <Gear></Gear>
-        </IconButton>
-      }
+          el.index === selected ? (
+            <Box sx={{backgroundColor: theme.palette.primary.main, borderRadius: 1.5}} key={el.index}>
+              <IconButton sx={{width:"max-content", color:"white"}}>{el.icon}</IconButton>
+            </Box>
+          ) : (
+            <IconButton onClick={()=>{
+              setSelected(el.index)
+            }} sx={{width:"max-content", color:"black"}} key={el.index}>{el.icon}</IconButton>
+          )
+        )}
+        <Divider sx={{width: "48px"}} />
+        {Nav_Setting.map((el) =>
+          el.index === selected ? (
+            <Box sx={{backgroundColor: theme.palette.primary.main, borderRadius: 1.5}} key={el.index}>
+              <IconButton sx={{width: "max-content", color: "white"}}>{el.icon}</IconButton>
+            </Box>
+          ) : (
+            <IconButton onClick={()=>{
+              setSelected(el.index)
+            }} sx={{width:"max-content", color:"black"}} key={el.index}>{el.icon}</IconButton>
+          )
+        )}
 
         
       </Stack>
@@ -66,3 +66,4 @@ const DashboardLayout = () => {
 };
 
 export default DashboardLayout;
+
