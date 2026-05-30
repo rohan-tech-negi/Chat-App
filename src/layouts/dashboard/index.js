5f33b5bd059eb5d1979050feff18633de661de56
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-30 18:32:05 +0530

    working on the another items of the sidebar to complete it

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,8 +1,9 @@
-import { Box, IconButton, Stack, useTheme } from "@mui/material";
+import { Box, Divider, IconButton, Stack, useTheme } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Nav_Buttons } from "../../data";
 import Logo from "../../assets/Images/logo.ico"
+import { Gear } from "phosphor-react";
 
 const DashboardLayout = () => {
 
@@ -14,7 +15,7 @@ const DashboardLayout = () => {
     <>
      <Box p={2} sx={({backgroundColor: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height: "100vh", width:100})}>
 
-      <Stack direction="column" sx={{width:"100%"}} alignItems={"center"}> 
+      <Stack direction="column" sx={{width:"100%"}} alignItems={"center"} spacing={3}> 
       <Box sx={{
         backgroundColor: theme.palette.primary.main, height: 64 , width: 64, borderRadius: 1.5
       }} >
@@ -25,7 +26,17 @@ const DashboardLayout = () => {
         </img>
 
       </Box>
-      {Nav_Buttons.map((el)=> <IconButton></IconButton>)}
+      <Stack spacing={3}>
+        {Nav_Buttons.map((el)=> <IconButton key={el.index}>{el.icon}</IconButton>)}
+        <Divider></Divider>
+
+        <IconButton>
+          <Gear></Gear>
+        </IconButton>
+      </Stack>
+
+      
+      
  </Stack>
      </Box>
       <Outlet />
