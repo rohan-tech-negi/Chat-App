187d87eb9032863faf4063b46dc7157d0fe389cb
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-30 18:25:01 +0530

    still be working on the setup of the sidebar components

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,4 +1,4 @@
-import { Box, useTheme } from "@mui/material";
+import { Box, Stack, useTheme } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
@@ -13,14 +13,19 @@ const DashboardLayout = () => {
   return (
     <>
      <Box sx={({backgroundColor: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height: "100vh", width:100})}>
+
+      <Stack direction="column" sx={{width:"100%"}} alignItems={"center"}> 
       <Box sx={{
         backgroundColor: theme.palette.primary.main, height: 64 , width: 64, borderRadius: 1.5
       }} >
+        
+
+       
         <img src={Logo} alt="chat app logo">
         </img>
 
       </Box>
-
+ </Stack>
      </Box>
       <Outlet />
     </>
