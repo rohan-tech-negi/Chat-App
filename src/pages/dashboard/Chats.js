e8443446f86d025af9de291e55ae5953eab4db05
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-01 11:03:36 +0530

    cretaed 3 componenets

--- src/pages/dashboard/Chats.js
+++ src/pages/dashboard/Chats.js
@@ -5,7 +5,29 @@ import React from 'react'
 const Search = styled("div")(({theme})=>({
   position: "relative",
   borderRadius: 20,
-  backgroundColor: alpha(theme.palette.background.paper, 1)
+  backgroundColor: alpha(theme.palette.background.paper, 1),
+  marginRight: theme.spacing(2),
+  width: "100%",
+  marginLeft: 0
+}))
+
+const SearchIconWrapper = styled("div")(({theme})=>({
+  padding: theme.spacing(0,2),
+  height: "100%",
+  position: "absolute",
+  display: "flex",
+  alignItems: "center",
+  pointerEvents: "none",
+  justifyContent: "center"
+}))
+
+const StyledInputBase = styled(InputBase)(({theme}) => ({
+  color: "inherit",
+  "& .MuiInputBase-input":{
+    padding: theme.spacing(1,1,1,0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    width: "100%"
+  }
 }))
 
 const Chats = () => {
