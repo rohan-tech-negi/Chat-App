8a151ebbb85a0428b8277ee167b4f77da24031a1
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-01 10:52:29 +0530

    started todays work with the chat section

--- src/pages/dashboard/Chats.js
+++ src/pages/dashboard/Chats.js
@@ -1,9 +1,18 @@
-import { Box } from '@mui/material'
+import { Box, IconButton, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const Chats = () => {
   return (
-    <Box sx={{position: "relative", height:"100%", width: 320, backgroundColor: "white", boxShadow:"0px 0px 2px rgba(0, 0, 0, 0.25)"}}>
+    <Box sx={{position: "relative", height:"100%", width: 320, backgroundColor: "#F8FAFF", boxShadow:"0px 0px 2px rgba(0, 0, 0, 0.25)"}}>
+
+        <Stack >
+            <Typography>
+                Chats
+            </Typography>
+            <IconButton>
+              
+            </IconButton>
+        </Stack>
 
     </Box>
   )
