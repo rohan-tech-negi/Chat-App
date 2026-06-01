a652cb963c13c4cc303291a7fc2674f60902a2d8
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-01 11:15:13 +0530

    making the avatar and the inside component of the chats component

--- src/pages/dashboard/Chats.js
+++ src/pages/dashboard/Chats.js
@@ -1,6 +1,36 @@
-import { Box, Button, Divider, IconButton, InputBase, Stack, Typography, alpha, styled } from '@mui/material'
+import { Avatar, Badge, Box, Button, Divider, IconButton, InputBase, Stack, Typography, alpha, styled } from '@mui/material'
 import {ArchiveBox, CircleDashed, MagnifyingGlass} from "phosphor-react"
-import React from 'react'
+import {Faker} from "@faker-js/faker"
+// import React from 'react'
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: 'ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}));
 
 const ChatElement= ()=>{
   return (
@@ -9,8 +39,8 @@ const ChatElement= ()=>{
       backgroundColor: "white",
       borderRadius: 1,
       height: 64
-    }}>
-      
+    }} p={2}>
+      <Avatar src={Faker.image} />
     </Box>
   )
 }
