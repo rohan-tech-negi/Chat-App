61534955ce46df98b4dee3a5979643b4833600af
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-03 08:02:52 +0530

    started our work on the general app

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,12 +1,17 @@
 import React, {Suspense, lazy} from "react";
 import Chats from "./Chats";
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 // const Cat = lazy(()=> import ('../../components/Cat.js'))
 const GeneralApp = () => {
 
   return (
     <Stack direction="row" sx={{ width: "100%" }}>
       <Chats></Chats>
+
+      {/* conversation */}
+      <Box>
+        
+      </Box>
     </Stack>
   );
 };
