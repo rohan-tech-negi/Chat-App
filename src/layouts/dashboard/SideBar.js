9c72b9b8e930e76fa154d717aec66f21a885d75d
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-09 19:25:38 +0530

    also fixed the import issues from the code

--- src/layouts/dashboard/SideBar.js
+++ src/layouts/dashboard/SideBar.js
@@ -1,8 +1,65 @@
-import { useTheme } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react';
+import { Box, IconButton, Stack, Divider, Avatar, Switch, useTheme } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { faker } from "@faker-js/faker";
+
+import Logo from "../../assets/Images/logo.ico";
+import { Nav_Buttons, Nav_Setting } from "../../data";
+import useSettings from "../../hooks/useSettings.js";
+
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 40,
+  height: 20,
+  padding: 0,
+  display: 'flex',
+  '&:active': {
+    '& .MuiSwitch-thumb': {
+      width: 15,
+    },
+    '& .MuiSwitch-switchBase.Mui-checked': {
+      transform: 'translateX(9px)',
+    },
+  },
+  '& .MuiSwitch-switchBase': {
+    padding: 2,
+    '&.Mui-checked': {
+      transform: 'translateX(12px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: '#1890ff',
+        // MUI v5 style dynamic check
+        ...(theme.palette.mode === 'dark' && {
+          backgroundColor: '#177ddc',
+        }),
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    transition: theme.transitions.create(['width'], {
+      duration: 200,
+    }),
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 20 / 2,
+    opacity: 1,
+    backgroundColor: 'rgba(0,0,0,.25)',
+    boxSizing: 'border-box',
+    // MUI v5 style dynamic check
+    ...(theme.palette.mode === 'dark' && {
+      backgroundColor: 'rgba(255,255,255,.35)',
+    }),
+  },
+}));
 
 const SideBar = () => {
-    const theme = useTheme()
+    const theme = useTheme();
+    const [selected, setSelected] = useState(0);
+    const { onToggleMode } = useSettings();
   return (
     <Box p={2} sx={{backgroundColor: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height: "100vh", width:100}}>
 
