b27cdfad71fdae273ff857b2c96e825d028c4d0b
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-09 23:34:03 +0530

    continuing the process

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -10,54 +10,7 @@ import useSettings from "../../hooks/useSettings.js"
 import SideBar from "./SideBar.js";
 
 
-const AntSwitch = styled(Switch)(({ theme }) => ({
-  width: 40,
-  height: 20,
-  padding: 0,
-  display: 'flex',
-  '&:active': {
-    '& .MuiSwitch-thumb': {
-      width: 15,
-    },
-    '& .MuiSwitch-switchBase.Mui-checked': {
-      transform: 'translateX(9px)',
-    },
-  },
-  '& .MuiSwitch-switchBase': {
-    padding: 2,
-    '&.Mui-checked': {
-      transform: 'translateX(12px)',
-      color: '#fff',
-      '& + .MuiSwitch-track': {
-        opacity: 1,
-        backgroundColor: '#1890ff',
-        // MUI v5 style dynamic check
-        ...(theme.palette.mode === 'dark' && {
-          backgroundColor: '#177ddc',
-        }),
-      },
-    },
-  },
-  '& .MuiSwitch-thumb': {
-    boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
-    width: 16,
-    height: 16,
-    borderRadius: 8,
-    transition: theme.transitions.create(['width'], {
-      duration: 200,
-    }),
-  },
-  '& .MuiSwitch-track': {
-    borderRadius: 20 / 2,
-    opacity: 1,
-    backgroundColor: 'rgba(0,0,0,.25)',
-    boxSizing: 'border-box',
-    // MUI v5 style dynamic check
-    ...(theme.palette.mode === 'dark' && {
-      backgroundColor: 'rgba(255,255,255,.35)',
-    }),
-  },
-}));
+
 
 
 const DashboardLayout = () => {
