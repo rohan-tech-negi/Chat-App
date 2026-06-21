31be0e3417a2346525ce043bb0eca554c2bfcdac
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-21 23:25:36 +0530

    setup the react redux as well

--- src/radux/store.js
+++ src/radux/store.js
@@ -1,4 +1,5 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {useDispatch as useAppDispatch, useSelector as useAppSelector} from "react-redux"
 
 const store = configureStore({
     reducer: {}
