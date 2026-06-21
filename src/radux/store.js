e287c61cf4e9a5e833dcf12f026ba414eed11405
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-21 23:27:29 +0530

    setup the redux persist as well

--- src/radux/store.js
+++ src/radux/store.js
@@ -1,5 +1,8 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {useDispatch as useAppDispatch, useSelector as useAppSelector} from "react-redux"
+import {persistStore, persistReducer} from "redux-persist"
+
+
 
 const store = configureStore({
     reducer: {}
