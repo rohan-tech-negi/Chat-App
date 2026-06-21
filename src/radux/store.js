6b2245a4e13f56d79819be29c176f9e267bf8997
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-22 00:14:15 +0530

    and also used the roots

--- src/radux/store.js
+++ src/radux/store.js
@@ -1,11 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {useDispatch as useAppDispatch, useSelector as useAppSelector} from "react-redux"
 import {persistStore, persistReducer} from "redux-persist"
+import { rootPersistConfig, rootReducer } from "./rootReducer";
 
 
 
 const store = configureStore({
-    reducer: persistReducer(),
+    reducer: persistReducer(rootPersistConfig, rootReducer),
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
         immutableCheck: false,
