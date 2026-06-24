261b802c094d95b0801eab17f6f25115b5f438eb
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-25 00:32:27 +0530

    projected all the items of the shortcuts

--- src/sections/settings/Shortcuts.js
+++ src/sections/settings/Shortcuts.js
@@ -1,7 +1,95 @@
-import { Dialog, DialogContent, DialogTitle, Grid, Slide } from '@mui/material'
+import { Button, Dialog, DialogContent, DialogTitle, Grid, Slide, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const Shortcuts = ({open, handleClose}) => {
+
+    const list = [
+  {
+    key: 0,
+    title: "Mark as unread",
+    combination: ["Cmd", "Shift", "U"],
+  },
+  {
+    key: 1,
+    title: "Mute",
+    combination: ["Cmd", "Shift", "M"],
+  },
+  {
+    key: 2,
+    title: "Archive Chat",
+    combination: ["Cmd", "Shift", "E"],
+  },
+  {
+    key: 3,
+    title: "Delete Chat",
+    combination: ["Cmd", "Shift", "D"],
+  },
+  {
+    key: 4,
+    title: "Pin Chat",
+    combination: ["Cmd", "Shift", "P"],
+  },
+  {
+    key: 5,
+    title: "Search",
+    combination: ["Cmd", "F"],
+  },
+  {
+    key: 6,
+    title: "Search Chat",
+    combination: ["Cmd", "Shift", "F"],
+  },
+  {
+    key: 7,
+    title: "Next Chat",
+    combination: ["Cmd", "N"],
+  },
+  {
+    key: 8,
+    title: "Next Step",
+    combination: ["Ctrl", "Tab"],
+  },
+  {
+    key: 9,
+    title: "Previous Step",
+    combination: ["Ctrl", "Shift", "Tab"],
+  },
+  {
+    key: 10,
+    title: "New Group",
+    combination: ["Cmd", "Shift", "N"],
+  },
+  {
+    key: 11,
+    title: "Profile & About",
+    combination: ["Cmd", "P"],
+  },
+  {
+    key: 12,
+    title: "Increase speed of voice message",
+    combination: ["Shift", "."],
+  },
+  {
+    key: 13,
+    title: "Decrease speed of voice message",
+    combination: ["Shift", ","],
+  },
+  {
+    key: 14,
+    title: "Settings",
+    combination: ["Shift", "S"],
+  },
+  {
+    key: 15,
+    title: "Emoji Panel",
+    combination: ["Cmd", "E"],
+  },
+  {
+    key: 16,
+    title: "Sticker Panel",
+    combination: ["Cmd", "S"],
+  },
+];
     const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
@@ -12,9 +100,39 @@ const Shortcuts = ({open, handleClose}) => {
             Keyboard Shortcuts
         </DialogTitle>
         <DialogContent sx={{mt:4}}>
-            <Grid container >
-
-            </Grid>
+            <Grid container spacing={3}>
+            {list.map(({ key, title, combination }) => {
+              return (
+                <Grid item xs={6}>
+                  <Stack
+                    sx={{ width: "100%" }}
+                    justifyContent="space-between"
+                    key={key}
+                    spacing={3}
+                    direction={"row"}
+                    alignItems="center"
+                  >
+                    <Typography variant="caption" sx={{ fontSize: 14 }}>
+                      {title}
+                    </Typography>
+                    <Stack spacing={2} direction="row">
+                      {combination.map((el) => {
+                        return (
+                          <Button
+                            sx={{ color: "#212121" }}
+                            disabled
+                            variant="contained"
+                          >
+                            {el}
+                          </Button>
+                        );
+                      })}
+                    </Stack>
+                  </Stack>
+                </Grid>
+              );
+            })}
+          </Grid>
         </DialogContent>
 
     </Dialog>
