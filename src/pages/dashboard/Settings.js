c07e0c2864ff58e19bcd6908db96775cb29e1b53
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-24 12:39:25 +0530

    building different sections of the settings tab

--- src/pages/dashboard/Settings.js
+++ src/pages/dashboard/Settings.js
@@ -1,11 +1,21 @@
-import { Stack } from '@mui/material'
+import { Box, Stack, useTheme } from '@mui/material'
 import React from 'react'
 
 const Settings = () => {
+  const theme = useTheme()
   return (
     <Stack direction={"row"} sx={{ width: "100%" }}>
       {/* Left Side */}
-      
+      <Box sx={{ overflowY: "scroll", height: "100vh", width: 320, boxShadow: "0 0 2px rgba(0, 0, 0, 0.25)", backgroundColor: theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background }}>
+        <Stack p={4} spacing={5}>
+          {/* header */}
+            
+          {/* profile */}
+
+          {/* list of options */}
+
+        </Stack>
+      </Box>
     </Stack>
   )
 }
