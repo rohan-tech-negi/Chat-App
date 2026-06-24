89431951ce3a7627de8aa07457d4374a6b1779cf
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-25 01:02:48 +0530

    done with todays work with the login page making

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,8 +1,16 @@
+import { Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <Stack spacing={2} sx={{mb:5 , position: "relative"}}>   
+      <Typography variant='h4'>Login to Tawk</Typography>
+      <Stack direction={}>
+
+      </Stack>
+    </Stack>
+    </>
   )
 }
 
