45334f30559058fa0a0086140d7d2c2a8adc0e9a
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-25 00:10:55 +0530

    using some list as per the usage

--- src/pages/dashboard/Settings.js
+++ src/pages/dashboard/Settings.js
@@ -5,6 +5,57 @@ import { faker } from "@faker-js/faker"
 
 const Settings = () => {
   const theme = useTheme()
+
+  const list = [
+    {
+      key: 0,
+      icon: <Bell size={20} />,
+      title: "Notifications",
+      onclick: () => {},
+    },
+    {
+      key: 1,
+      icon: <Lock size={20} />,
+      title: "Privacy",
+      onclick: () => {},
+    },
+    {
+      key: 2,
+      icon: <Key size={20} />,
+      title: "Security",
+      onclick: () => {},
+    },
+    {
+      key: 3,
+      icon: <PencilCircle size={20} />,
+      title: "Theme",
+      onclick: handleOpenTheme,
+    },
+    {
+      key: 4,
+      icon: <Image size={20} />,
+      title: "Chat Wallpaper",
+      onclick: () => {},
+    },
+    {
+      key: 5,
+      icon: <Note size={20} />,
+      title: "Request Account Info",
+      onclick: () => {},
+    },
+    {
+      key: 6,
+      icon: <Keyboard size={20} />,
+      title: "Keyboard Shortcuts",
+      onclick: handleOpenShortcuts,
+    },
+    {
+      key: 7,
+      icon: <Info size={20} />,
+      title: "Help",
+      onclick: () => {},
+    },
+  ];
   return (
     <Stack direction={"row"} sx={{ width: "100%" }}>
       {/* Left Side */}
