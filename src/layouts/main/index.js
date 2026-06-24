ee5639c9410f9b88f84539df19379e524fb86813
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-25 00:57:44 +0530

    now be making the login page

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -1,6 +1,7 @@
 import { Container, Stack } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Logo from "../../assets/Images/logo.ico"
 
 const MainLayout = () => {
   return (
@@ -8,10 +9,11 @@ const MainLayout = () => {
     <Container sx={{mt:5}} maxWidth ="sm">
       <Stack spacing={5}>
         <Stack sx={{width: "100vh"}} direction="column" alignItems={"center"}>
-
+          <img style={{height: 120, width: 120}} src={Logo} >
+           </img>
         </Stack>
       </Stack>
-      <div>Main Layout</div>
+      {/* <div>Main Layout</div> */}
 
       <Outlet />
     </Container>
