766b76144984495d5749ae6ab6f163f5a89542d8
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-25 00:53:57 +0530

    making the ui of the mainlayout

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -1,12 +1,20 @@
+import { Container, Stack } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
     <>
+    <Container sx={{mt:5}} maxWidth ="sm">
+      <Stack spacing={5}>
+        <Stack sx={{width: "100vh"}} direction="column" alignItems={"center"}>
+
+        </Stack>
+      </Stack>
       <div>Main Layout</div>
 
       <Outlet />
+    </Container>
     </>
   );
 };
