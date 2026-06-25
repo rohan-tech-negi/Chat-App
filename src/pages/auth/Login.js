4533c70d9c0ae6c07ccb39bf7c900e1195030840
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-26 03:35:26 +0530

    started our todays work with the login page

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,13 +1,16 @@
 import { Stack, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <>
     <Stack spacing={2} sx={{mb:5 , position: "relative"}}>   
       <Typography variant='h4'>Login to Tawk</Typography>
-      <Stack direction={}>
-
+      <Stack direction="row" spacing={0.5}>
+          <Typography variant='body2'>New User?</Typography>
+          <Link>
+          </Link>
       </Stack>
     </Stack>
     </>
