6e2a1c1441fb5f59cd0c7f0e86dbe322d1ca19a2
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-26 03:56:52 +0530

    making the fileds of the form

--- src/components/hook-form/RHFtextField.js
+++ src/components/hook-form/RHFtextField.js
@@ -6,10 +6,15 @@ import {useFormContext, Controller} from "react-hook-form"
 import { TextField } from "@mui/material";
 
 
-export default function RHFTextField({}){
+export default function RHFTextField({name, helperText, ...other}){
+    const {control} = useFormContext
     return (
-        <>
-            
-        </>
+        <Controller name={name} control={control} render={({field, fieldState: {error}})=>(
+            <TextField {...field} fullWidth error={!!error}>
+
+            </TextField>
+        )}>
+
+        </Controller>
     )
 }
