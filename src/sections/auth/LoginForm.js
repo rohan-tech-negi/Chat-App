21da63b3cb85328fd0c1f10627f85bd37af9301d
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-26 04:10:18 +0530

    done with the logic part of the login form

--- src/sections/auth/LoginForm.js
+++ src/sections/auth/LoginForm.js
@@ -2,6 +2,7 @@ import React from 'react'
 import * as Yup from "yup";
 
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import FormProvider from '../../components/hook-form/FormProvider'
 import { useState } from 'react';
 
@@ -15,6 +16,38 @@ const LoginForm = () => {
       .email("Email must be a valid email address"),
     password: Yup.string().required("Password is required"),
   });
+
+    const defaultValues = {
+    email: "demo@tawk.com",
+    password: "demo1234",
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
+
+    const {
+    reset,
+    setError,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+
+   const onSubmit = async (data) => {
+    try {
+      console.log(data);
+      // submit data to backend
+      
+    } catch (error) {
+      console.error(error);
+      reset();
+      setError("afterSubmit", {
+        ...error,
+        message: error.message,
+      });
+    }
+  };
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
 
