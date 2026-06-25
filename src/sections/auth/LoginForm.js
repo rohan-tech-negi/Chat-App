472a1ac54e2c28923661d1cf48dca0d41c995035
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-26 04:07:10 +0530

    working on the logic section of the loginform

--- src/sections/auth/LoginForm.js
+++ src/sections/auth/LoginForm.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import * as Yup from "yup";
+
+import { useForm } from "react-hook-form";
 import FormProvider from '../../components/hook-form/FormProvider'
+import { useState } from 'react';
 
 const LoginForm = () => {
+
+  const [showPassword, setShowPassword] = useState(false);
+
+   const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+      .required("Email is required")
+      .email("Email must be a valid email address"),
+    password: Yup.string().required("Password is required"),
+  });
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
 
