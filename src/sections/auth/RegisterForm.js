657927fa2904eb72fa93b0274a604848e11592f2
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-28 14:37:05 +0530

    first we be making the logic part that we be making the ui part

--- src/sections/auth/RegisterForm.js
+++ src/sections/auth/RegisterForm.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import {useState} from 'react'
+import * as Yup from 'yup'
+import {useDispatch, useSelector} from "react-redux"
+
 
 const RegisterForm = () => {
+     const dispatch = useDispatch();
+  const {isLoading} = useSelector((state) => state.auth);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const LoginSchema = Yup.object().shape({
+    firstName: Yup.string().required("First name required"),
+    lastName: Yup.string().required("Last name required"),
+    email: Yup.string()
+      .required("Email is required")
+      .email("Email must be a valid email address"),
+    password: Yup.string().required("Password is required"),
+  });
+   const defaultValues = {
+    firstName: "",
+    lastName: "",
+    email: "demo@tawk.com",
+    password: "demo1234",
+  };
   return (
     <div>RegisterForm</div>
   )
