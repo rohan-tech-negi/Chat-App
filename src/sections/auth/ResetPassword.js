85f94869da3d96f2118b33b3f76b749055a82cf6
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-28 14:50:27 +0530

    done with todays work

--- src/sections/auth/ResetPassword.js
+++ src/sections/auth/ResetPassword.js
@@ -1,9 +1,20 @@
+import { Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const ResetPassword = () => {
   return (
 <>
+<Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
+        <Typography variant="h3" paragraph>
+          Forgot your password?
+        </Typography>
 
+        <Typography sx={{ color: "text.secondary", mb: 5 }}>
+          Please enter the email address associated with your account and We
+          will email you a link to reset your password.
+        </Typography>
+        
+      </Stack>
 </>
   )
 }
