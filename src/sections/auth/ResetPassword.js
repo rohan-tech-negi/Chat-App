e012374ab5a6096ea67482bc17cacdd2c048fdfb
rohan-tech-negi <rohanwork953@gmail.com>
2026-06-29 15:14:58 +0530

    done our work with the resetpassword page

--- src/sections/auth/ResetPassword.js
+++ src/sections/auth/ResetPassword.js
@@ -1,5 +1,7 @@
-import { Stack, Typography } from '@mui/material'
+import { Link, Stack, Typography } from '@mui/material'
+import { CaretLeft } from 'phosphor-react'
 import React from 'react'
+import {Link as RouterLink} from 'react-router-dom'
 
 const ResetPassword = () => {
   return (
@@ -15,6 +17,22 @@ const ResetPassword = () => {
         </Typography>
         
       </Stack>
+
+      <Link
+        component={RouterLink}
+        to={"/auth/login"}
+        color="inherit"
+        variant="subtitle2"
+        sx={{
+          mt: 3,
+          mx: "auto",
+          alignItems: "center",
+          display: "inline-flex",
+        }}
+      >
+        <CaretLeft size={24} />
+        Return to sign in
+      </Link>
 </>
   )
 }
