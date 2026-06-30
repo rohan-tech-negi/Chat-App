19a2355b49fe9b37292cd779b59be0317014d08a
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-01 01:23:41 +0530

    first we work on the ui that on the logic part

--- src/sections/main/CreateGroup.js
+++ src/sections/main/CreateGroup.js
@@ -1,8 +1,21 @@
+import { Dialog } from '@mui/material'
 import React from 'react'
 
 const CreateGroup = () => {
   return (
-    <div>CreateGroup</div>
+    <Dialog
+      fullWidth
+      maxWidth="xs"
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+      sx={{ p: 4 }}
+    >
+
+        
+    </Dialog>
   )
 }
 
