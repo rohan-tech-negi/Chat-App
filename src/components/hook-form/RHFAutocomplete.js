b6b2f4fec3a59736f5fdb3681fed86c65177a7d4
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-01 01:50:00 +0530

    done with the autocomplete file

--- src/components/hook-form/RHFAutocomplete.js
+++ src/components/hook-form/RHFAutocomplete.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import PropTypes from 'prop-types';
+// form
+import { useFormContext, Controller } from 'react-hook-form';
+// @mui
+import { Autocomplete, TextField } from '@mui/material';
 
-const RHFAutocomplete = () => {
+// ----------------------------------------------------------------------
+
+RHFAutocomplete.propTypes = {
+  name: PropTypes.string,
+  label: PropTypes.string,
+  helperText: PropTypes.node,
+};
+
+export default function RHFAutocomplete({ name, label, helperText, ...other }) {
+  const { control, setValue } = useFormContext();
+
   return (
-    <div>RHFAutocomplete</div>
-  )
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <Autocomplete
+          {...field}
+          onChange={(event, newValue) => setValue(name, newValue, { shouldValidate: true })}
+          renderInput={(params) => (
+            <TextField
+              label={label}
+              error={!!error}
+              helperText={error ? error?.message : helperText}
+              {...params}
+            />
+          )}
+          {...other}
+        />
+      )}
+    />
+  );
 }
-
-export default RHFAutocomplete
