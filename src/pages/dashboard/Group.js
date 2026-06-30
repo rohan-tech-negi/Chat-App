e3dd2dffe34b369910267a20a9f4f71a8e8c71b8
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-01 00:41:36 +0530

    continuing the work on the left side of the group.js file

--- src/pages/dashboard/Group.js
+++ src/pages/dashboard/Group.js
@@ -1,9 +1,21 @@
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, IconButton, Stack, Typography, useTheme } from '@mui/material'
 import React from 'react'
 import { Search, SearchIconWrapper, StyledInputBase } from '../../components/Search'
-import { MagnifyingGlass } from 'phosphor-react'
+import { Link, MagnifyingGlass, Plus } from 'phosphor-react'
+import { useState } from 'react'
 
 const Group = () => {
+
+   const [openDialog, setOpenDialog] = useState(false);
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  }
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  }
+
+    const theme = useTheme();
     
   return (
     <>
@@ -43,6 +55,19 @@ const Group = () => {
                   inputProps={{ "aria-label": "search" }}
                 />
               </Search>
+            </Stack>
+
+            <Stack
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              direction={"row"}
+            >
+              <Typography variant="subtitle2" sx={{}} component={Link}>
+                Create New Group
+              </Typography>
+              <IconButton onClick={handleOpenDialog}>
+                <Plus style={{ color: theme.palette.primary.main }} />
+              </IconButton>
             </Stack>
 
 
