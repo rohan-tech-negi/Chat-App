8f8dc0293581eb5bedece8eace4700e5842c202f
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-03 01:45:54 +0530

    putted some conditoons

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,13 +1,17 @@
 import { Stack } from "@mui/material";
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import SideBar from "./SideBar.js";
 
 
 
-
+const isAuthenticated = true;
 
 const DashboardLayout = () => {
+  // const { isAuthenticated } = useAuth();
+  if(!isAuthenticated){
+    return <Navigate to={"/auth/login"} replace />;
+  }
 
   
 
