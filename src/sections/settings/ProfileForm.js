69680e4f3fe17ccf55ac3980192d4db9ef9ecb08
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-03 01:22:33 +0530

    setup the profile form corectly

--- src/sections/settings/ProfileForm.js
+++ src/sections/settings/ProfileForm.js
@@ -12,18 +12,19 @@ import { Link as RouterLink } from "react-router-dom";
 
 const ProfileForm = () => {
 
-  const [showPassword, setShowPassword] = useState(false);
+ 
 
    const LoginSchema = Yup.object().shape({
-    email: Yup.string()
-      .required("Email is required")
-      .email("Email must be a valid email address"),
-    password: Yup.string().required("Password is required"),
+    name: Yup.string()
+      .required("name is required"),
+    about: Yup.string().required("about is required"),
+
+    avatarUrl : Yup.string().require("Avatar is required").nullable(true)
   });
 
     const defaultValues = {
-    email: "demo@tawk.com",
-    password: "demo1234",
+    name: "",
+    about: "",
   };
 
   const methods = useForm({
@@ -33,15 +34,21 @@ const ProfileForm = () => {
 
     const {
     reset,
+    watch,
+    control,
     setError,
     handleSubmit,
     formState: { errors },
   } = methods;
 
+
+  
+
    const onSubmit = async (data) => {
     try {
       console.log(data);
       // submit data to backend
+      console.log("Data", data)
       
     } catch (error) {
       console.error(error);
@@ -59,52 +66,13 @@ const ProfileForm = () => {
           <Alert severity="error">{errors.afterSubmit.message}</Alert>
         )}
         <RHFTextField name="email" label="Email address" />
-<RHFTextField
-          name="password"
-          label="Password"
-          type={showPassword ? "text" : "password"}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  onClick={() => setShowPassword(!showPassword)}
-                  edge="end"
-                >
-                  {showPassword ? <Eye /> : <EyeSlash />}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
+
 
 
 
 </Stack>
- <Stack alignItems="flex-end" sx={{ my: 2 }}>
-        <Link  component={RouterLink} to="/auth/reset-password" variant="body2" color="inherit" underline="always">
-          Forgot password?
-        </Link>
-      </Stack>
- <Button
-        fullWidth
-        color="inherit"
-        size="large"
-        type="submit"
-        variant="contained"
-        // loading={isLoading}
-        sx={{
-          bgcolor: "text.primary",
-          color: (theme) =>
-            theme.palette.mode === "light" ? "common.white" : "grey.800",
-          "&:hover": {
-            bgcolor: "text.primary",
-            color: (theme) =>
-              theme.palette.mode === "light" ? "common.white" : "grey.800",
-          },
-        }}
-      >
-        Login
-      </Button>
+
+ 
       
     </FormProvider>
   )
