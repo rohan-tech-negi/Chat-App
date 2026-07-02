b4be249a53e6abc2d26b35ed81248e34e3685a04
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-03 02:30:58 +0530

    setting up various packages

--- chat-server/app.js
+++ chat-server/app.js
@@ -4,5 +4,17 @@ const app = express()
 
 const morgan = require("morgan")
 
+const rateLimit = require("express-rate-limit")
+
+
+const helmet = require("helmet")
+
+const mongosanitize = require("express-mongo-sanitize")
+
+
+
+
+
+
 
 module.exports = app;
