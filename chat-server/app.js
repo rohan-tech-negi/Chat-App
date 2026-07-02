3af3586ff043bf5ecda89753bed29f6ddaf53173
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-03 02:40:15 +0530

    done with the modules setup

--- chat-server/app.js
+++ chat-server/app.js
@@ -12,8 +12,38 @@ const helmet = require("helmet")
 const mongosanitize = require("express-mongo-sanitize")
 
 
+const bodyParser = require("body-parser")
+
+const xss = require("xss")
 
 
+
+
+
+
+app.use(express.json({limit: "10kb"}))
+
+app.use(express.urlencoded({extended:true,limit:"10kb"}))
+
+app.use(bodyParser.json())
+
+app.use(helmet())
+
+if(process.env.NODE_ENV === "development"){
+
+    app.use(morgan("dev"))
+
+}
+
+const limiter = rateLimit({
+    max:3000,
+    windowMs:60*60*1000,
+    message:"Too many requests from this IP, please try again in an hour"
+})
+
+app.use("/api",limiter)
+app.use(xss())
+app.use(mongosanitize())
 
 
 
