a9db6bed79c9cf3363a358ffb2ecbc1d4a89ebf5
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-06 00:34:24 +0530

    created the first controlle rnamed login controller

--- chat-server/controllers/auth.js
+++ chat-server/controllers/auth.js
@@ -1,1 +1,48 @@
 const jwt = require("jsonwebtoken");
+
+
+
+const User = require("../models/User.js")
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // console.log(email, password);
+
+  if (!email || !password) {
+    res.status(400).json({
+      status: "error",
+      message: "Both email and password are required",
+    });
+    return;
+  }
+
+  const user = await User.findOne({ email: email }).select("+password");
+
+  if (!user || !user.password) {
+    res.status(400).json({
+      status: "error",
+      message: "Incorrect password",
+    });
+
+    return;
+  }
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    res.status(400).json({
+      status: "error",
+      message: "Email or password is incorrect",
+    });
+
+    return;
+  }
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: "success",
+    message: "Logged in successfully!",
+    token,
+    user_id: user._id,
+  });
+});
