9361be972b51a82c9f38b744491b3bd365bd4a81
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-06 02:22:36 +0530

    a new function in the user model

--- chat-server/models/User.js
+++ chat-server/models/User.js
@@ -80,6 +80,20 @@ const userSchema = new mongoose.Schema({
 });
 
 
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("otp") || !this.otp) return next();
+
+  // Hash the otp with cost of 12
+  this.otp = await bcrypt.hash(this.otp.toString(), 12);
+
+  console.log(this.otp.toString(), "FROM PRE SAVE HOOK");
+
+  next();
+});
+
+
+
 
 userSchema.methods.correctPassword = async function (
   candidatePassword,
