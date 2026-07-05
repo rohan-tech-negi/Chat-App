9b4cb0a68c400877e011da7aa8129a90c340a43b
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-05 17:36:39 +0530

    setting up bd connection methods

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,13 +1,31 @@
 const app = require("./app");
+const dotenv = require("dotenv")
+dotenv.config({path: "./config.env"})
 
-process.on("uncaughtException")
+process.on("uncaughtException", (err)=>{
+  console.log(err);process.env(1)
+})
+
+
+
 
 const http = require("http");
 
 const server = http.createServer(app);
+
+
+const DB = process.env.DBURI.replace("<PASSWORD>", process.env.DBPASSWORD);
 
 const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
+
+
+process.on("unhandledRejection",(err)=>{
+  console.log(err)
+  server.close(()=>{
+    process.exit(1)
+  })
+})
