c740f02d9dc55ff82ffd80fae999a50ffb201903
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-06 02:20:53 +0530

    created the verifyOTP controller

--- chat-server/controllers/auth.js
+++ chat-server/controllers/auth.js
@@ -104,27 +104,78 @@ exports.sendOTP = catchAsync(async (req, res, next) => {
 
   const otp_expiry_time = Date.now() + 10 * 60 * 1000; // 10 Mins after otp is sent
 
-  const user = await User.findByIdAndUpdate(userId, {
+  await User.findByIdAndUpdate(userId, {
     otp_expiry_time: otp_expiry_time,
   });
 
-  user.otp = new_otp.toString();
+//   user.otp = new_otp.toString();
 
-  await user.save({ new: true, validateModifiedOnly: true });
+//   await user.save({ new: true, validateModifiedOnly: true });
 
-  console.log(new_otp);
+//   console.log(new_otp);
 
-  // TODO send mail
-  mailService.sendEmail({
-    from: "shreyanshshah242@gmail.com",
-    to: user.email,
-    subject: "Verification OTP",
-    html: otp(user.firstName, new_otp),
-    attachments: [],
-  });
+//   // TODO send mail
+//   mailService.sendEmail({
+//     from: "shreyanshshah242@gmail.com",
+//     to: user.email,
+//     subject: "Verification OTP",
+//     html: otp(user.firstName, new_otp),
+//     attachments: [],
+//   });
 
   res.status(200).json({
     status: "success",
     message: "OTP Sent Successfully!",
+  });
+});
+
+
+
+
+
+exports.verifyOTP = catchAsync(async (req, res, next) => {
+  // verify otp and update user accordingly
+  const { email, otp } = req.body;
+  const user = await User.findOne({
+    email,
+    otp_expiry_time: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      status: "error",
+      message: "Email is invalid or OTP expired",
+    });
+  }
+
+  if (user.verified) {
+    return res.status(400).json({
+      status: "error",
+      message: "Email is already verified",
+    });
+  }
+
+  if (!(await user.correctOTP(otp, user.otp))) {
+    res.status(400).json({
+      status: "error",
+      message: "OTP is incorrect",
+    });
+
+    return;
+  }
+
+  // OTP is correct
+
+  user.verified = true;
+  user.otp = undefined;
+  await user.save({ new: true, validateModifiedOnly: true });
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: "success",
+    message: "OTP verified Successfully!",
+    token,
+    user_id: user._id,
   });
 });
