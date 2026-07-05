b396614229751842df14daf1d98c8bc785f8d90a
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-06 02:04:39 +0530

    created another controller register

--- chat-server/controllers/auth.js
+++ chat-server/controllers/auth.js
@@ -7,6 +7,48 @@ const User = require("../models/user.js")
 
 const signToken = (userId) => jwt.sign({ userId }, process.env.JWT_SECRET);
 
+exports.register = catchAsync(async (req, res, next) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  const filteredBody = filterObj(
+    req.body,
+    "firstName",
+    "lastName",
+    "email",
+    "password"
+  );
+
+  // check if a verified user with given email exists
+
+  const existing_user = await User.findOne({ email: email });
+
+  if (existing_user && existing_user.verified) {
+    // user with this email already exists, Please login
+    return res.status(400).json({
+      status: "error",
+      message: "Email already in use, Please login.",
+    });
+  } else if (existing_user) {
+    // if not verified than update prev one
+
+    await User.findOneAndUpdate({ email: email }, filteredBody, {
+      new: true,
+      validateModifiedOnly: true,
+    });
+
+    // generate an otp and send to email
+    req.userId = existing_user._id;
+    next();
+  } else {
+    // if user is not created before than create a new one
+    const new_user = await User.create(filteredBody);
+
+    // generate an otp and send to email
+    req.userId = new_user._id;
+    next();
+  }
+});
+
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
