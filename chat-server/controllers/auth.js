ed2ab550ea45e94aeab9b1808dd7c311d6878387
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-07 09:08:53 +0530

    starting our wwork with the auth.js controller protect controller

--- chat-server/controllers/auth.js
+++ chat-server/controllers/auth.js
@@ -230,3 +230,48 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     });
   }
 });
+
+
+
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check if it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+
+  if (!token) {
+    return res.status(401).json({
+      message: "You are not logged in! Please log in to get access.",
+    });
+  }
+  // 2) Verification of token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  console.log(decoded);
+
+  // 3) Check if user still exists
+
+  const this_user = await User.findById(decoded.userId);
+  if (!this_user) {
+    return res.status(401).json({
+      message: "The user belonging to this token does no longer exists.",
+    });
+  }
+  // 4) Check if user changed password after the token was issued
+  if (this_user.changedPasswordAfter(decoded.iat)) {
+    return res.status(401).json({
+      message: "User recently changed password! Please log in again.",
+    });
+  }
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = this_user;
+  next();
+});
