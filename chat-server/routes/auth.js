a4dcb0342605721ad3863cefbf552c0bebeb0ca9
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-07 14:34:11 +0530

    created all the routes in the auth.js

--- chat-server/routes/auth.js
+++ chat-server/routes/auth.js
@@ -2,6 +2,18 @@ const router = require("express").Router()
 
 const authController = require("../controllers/auth.js")
 
-router.post("login", authController.login)
+
+
+router.post("/login", authController.login)
+router.post("/register", authController.register)
+
+router.post("/send-otp", authController.sendOTP)
+
+router.post("/verify-otp", authController.verifyOTP)
+
+router.post("/forget-password", authController.forgotPassword)
+
+router.post("/reset-password", authController.resetPassword)
+
 
 module.exports = router
--- chat-server/routes/user.js
+++ chat-server/routes/user.js
@@ -1,5 +1,8 @@
 const router = require("express").Router()
 
 const userController = require("../controllers/user.js")
+const authController = require("../controllers/auth.js")
 
-router.post("/update-me", userController.updateMe)
+router.post("/update-me", authController.protect , userController.updateMe)
+
+module.exports = router;
