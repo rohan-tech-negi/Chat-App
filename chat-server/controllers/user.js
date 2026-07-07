4adf8e2380448a4f84d73c17f84c2e6ea8668cf8
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-07 09:36:07 +0530

    working on the first controller named uepdateme in the usercontroller

---
+++ chat-server/controllers/user.js
@@ -0,0 +1,20 @@
+const User = require("../models/user.js")
+
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  const filteredBody = filterObj(
+    req.body,
+    "firstName",
+    "lastName",
+    "about",
+    "avatar"
+  );
+
+  const userDoc = await User.findByIdAndUpdate(req.user._id, filteredBody);
+
+  res.status(200).json({
+    status: "success",
+    data: userDoc,
+    message: "User Updated successfully",
+  });
+});
