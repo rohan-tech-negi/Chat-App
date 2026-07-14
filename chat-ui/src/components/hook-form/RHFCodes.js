e9aea313955f91ced4ecf37b8bab36edc1c55757
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-15 01:56:40 +0530

    creaetd the jsx part

--- chat-ui/src/components/hook-form/RHFCodes.js
+++ chat-ui/src/components/hook-form/RHFCodes.js
@@ -1,8 +1,49 @@
 import React from 'react'
 
-const RHFCodes = () => {
+import { useRef } from "react";
+import { Stack, TextField } from "@mui/material";
+import { useFormContext, Controller } from "react-hook-form";
+
+
+const RHFCodes = ({ keyName = "", inputs = [], ...other }) => {
+    const codesRef = useRef(null);
+
+     const { control } = useFormContext();
   return (
-    <div>RHFCodes</div>
+     <Stack direction="row" spacing={2} justifyContent="center" ref={codesRef}>
+          {inputs.map((name, index) => (
+        <Controller
+          key={name}
+          name={`${keyName}${index + 1}`}
+          control={control}
+          render={({ field, fieldState: { error } }) => (
+            <TextField
+              {...field}
+              error={!!error}
+              autoFocus={index === 0}
+              placeholder="-"
+              onChange={(event) => {
+                handleChangeWithNextField(event, field.onChange);
+              }}
+              onFocus={(event) => event.currentTarget.select()}
+              InputProps={{
+                sx: {
+                  width: { xs: 36, sm: 56 },
+                  height: { xs: 36, sm: 56 },
+                  "& input": { p: 0, textAlign: "center" },
+                },
+              }}
+              inputProps={{
+                maxLength: 1,
+                type: "number",
+              }}
+              {...other}
+            />
+          )}
+        />
+      ))}
+
+     </Stack>
   )
 }
 
