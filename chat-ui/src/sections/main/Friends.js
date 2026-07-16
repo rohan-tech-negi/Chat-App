12d8d2253473a9ecee8ac856c73bf747c3c2bc5c
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-16 14:13:26 +0530

    friends.js page is divided in several parts so the first section is friends

--- chat-ui/src/sections/main/Friends.js
+++ chat-ui/src/sections/main/Friends.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
-const Friends = () => {
+const Friends = ({ open, handleClose }) => {
+
+     const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div>Friends</div>
   )
