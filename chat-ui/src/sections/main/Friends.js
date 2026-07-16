dbc9572fed9c67138d8d2f7b37e0b37308fd75a7
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-16 14:16:20 +0530

    done with the friends main section

--- chat-ui/src/sections/main/Friends.js
+++ chat-ui/src/sections/main/Friends.js
@@ -1,4 +1,4 @@
-import { Dialog, Stack } from '@mui/material';
+import { Dialog, DialogContent, Stack, Tab, Tabs } from '@mui/material';
 import React from 'react'
 
 const Friends = ({ open, handleClose }) => {
@@ -21,10 +21,36 @@ const Friends = ({ open, handleClose }) => {
     >
 
         <Stack p={2} sx={{ width: "100%" }}>
-            
+            <Tabs value={value} onChange={handleChange} centered>
+          <Tab label="Explore" />
+          <Tab label="Friends" />
+          <Tab label="Requests" />
+        </Tabs>
 
 
         </Stack>
+
+        <DialogContent>
+        <Stack sx={{ height: "100%" }}>
+          <Stack spacing={2.4}>
+            {(() => {
+              switch (value) {
+                case 0: // display all users in this list
+                  return <UsersList />;
+
+                case 1: // display friends in this list
+                  return <FriendsList />;
+
+                case 2: // display request in this list
+                  return <RequestsList />;
+
+                default:
+                  break;
+              }
+            })()}
+          </Stack>
+        </Stack>
+      </DialogContent>
     </Dialog>
   )
 }
