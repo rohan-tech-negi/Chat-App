b9d551b7b7fc6b86dd3bc23cb66f10e32613f52c
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-16 14:22:38 +0530

    creating seperate thunk actions

--- chat-ui/src/radux/slices/app.js
+++ chat-ui/src/radux/slices/app.js
@@ -27,9 +27,9 @@ const slice = createSlice({
           updateUsers(state, action) {
       state.users = action.payload.users;
     },
-    updateAllUsers(state, action) {
-      state.all_users = action.payload.users;
-    },
+    // updateAllUsers(state, action) {
+    //   state.all_users = action.payload.users;
+    // },
     updateFriends(state, action) {
       state.friends = action.payload.friends;
     },
@@ -57,6 +57,31 @@ export function UpdateSidebarType(type){
     return async(dispatch, getState)=>{
         dispatch(slice.actions.updateSidebarType({type}));
     }
+}
+
+
+
+export function FetchUsers() {
+  return async (dispatch, getState) => {
+    await axios
+      .get(
+        "/user/get-users",
+
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getState().auth.token}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(slice.actions.updateUsers({ users: response.data.data }));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 }
 
 
