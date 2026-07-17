2df056443de7aee3466c53d3a0c4dbd45f7c954b
rohan-tech-negi <rohanwork953@gmail.com>
2026-07-17 08:37:52 +0530

    done with the index.js changes

--- chat-ui/src/layouts/dashboard/index.js
+++ chat-ui/src/layouts/dashboard/index.js
@@ -2,46 +2,57 @@ import { Stack } from "@mui/material";
 import React, { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import SideBar from "./SideBar.js";
-import {useSelector} from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { connectSocket, socket } from "../../socket.js";
-
-
+import toast from "react-hot-toast";
 
 const isAuthenticated = true;
 
 const DashboardLayout = () => {
-  const {isLoggedIn} = useSelector((state)=> state.auth)
+  const dispatch = useDispatch();
+  const { isLoggedIn } = useSelector((state) => state.auth);
 
-  const user_id = window.localStorage.getItem("user_id")  
+  const user_id = window.localStorage.getItem("user_id");
 
-  useEffect(()=>{
-    if(isLoggedIn){
-      window.onload = function(){
-        if(!window.location.hash){
-          window.location = window.location + '#loaded'
-          window.location.reload()
+  useEffect(() => {
+    if (isLoggedIn) {
+      window.onload = function () {
+        if (!window.location.hash) {
+          window.location = window.location + "#loaded";
+          window.location.reload();
         }
+      };
+
+      window.reload();
+      if (!socket) {
+        connectSocket(user_id);
       }
+      socket.on("new_friend_request", (data) => {
+        toast.success("New friend request received");
+      });
 
-      window.reload()
-      if(!socket){
-        connectSocket(user_id)
-      }
-
-
-      
+      socket.on("request_accepted", (data) => {
+        toast.success("friend request accepted");
+      });
+      socket.on("request_sent", (data) => {
+        toast.success("friend request accepted");
+      });
     }
-  },[isLoggedIn, socket])
-  if(!isLoggedIn){
+
+    return () => {
+      socket.off("new_friend_request");
+      socket.off("request_accepted");
+      socket.off("request_sent");
+    };
+  }, [isLoggedIn, socket]);
+  if (!isLoggedIn) {
     return <Navigate to={"/auth/login"} replace />;
   }
 
-  
-
   return (
     <Stack direction="row">
-     {/* sidebar */}
-     <SideBar></SideBar>
+      {/* sidebar */}
+      <SideBar></SideBar>
       <Outlet />
     </Stack>
   );
