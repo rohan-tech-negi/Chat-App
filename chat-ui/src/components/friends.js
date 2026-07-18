9cdef7c0e24263282c8cd8bcde6555cb7c7e2881
Rohan Singh Negi <rohanwork953@gmail.com>
2026-07-19 00:20:54 +0530

    resolved few errors

--- chat-ui/src/components/friends.js
+++ chat-ui/src/components/friends.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { useTheme , styled, Box, Badge} from '@mui/material';
+import { useTheme , styled, Box, Badge, Stack, Avatar, Typography, Button} from '@mui/material';
+import StyledBadge from "./StyledBadge.js"
+import { socket } from '../socket';
 
 const StyledChatBox = styled(Box)(({ theme }) => ({
   "&:hover": {
@@ -7,38 +9,65 @@ const StyledChatBox = styled(Box)(({ theme }) => ({
   },
 }));
 
-const StyledBadge = styled(Badge)(({ theme }) => ({
-  "& .MuiBadge-badge": {
-    backgroundColor: "#44b700",
-    color: "#44b700",
-    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    "&::after": {
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%",
-      borderRadius: "50%",
-      animation: "ripple 1.2s infinite ease-in-out",
-      border: "1px solid currentColor",
-      content: '""',
-    },
-  },
-  "@keyframes ripple": {
-    "0%": {
-      transform: "scale(.8)",
-      opacity: 1,
-    },
-    "100%": {
-      transform: "scale(2.4)",
-      opacity: 0,
-    },
-  },
-}));
 
-const UserElement = () => {
+
+const UserElement = ({ img, firstName, lastName, online, _id }) => {
+    const user_id = window.localStorage.getItem("user_id")
+    const theme = useTheme();
+
+  const name = `${firstName} ${lastName}`;
   return (
-    <div>friends</div>
+    <>
+        
+
+  return (
+    <StyledChatBox
+      sx={{
+        width: "100%",
+
+        borderRadius: 1,
+
+        backgroundColor: theme.palette.background.paper,
+      }}
+      p={2}
+    >
+      <Stack
+        direction="row"
+        alignItems={"center"}
+        justifyContent="space-between"
+      >
+        <Stack direction="row" alignItems={"center"} spacing={2}>
+          {" "}
+          {online ? (
+            <StyledBadge
+              overlap="circular"
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              variant="dot"
+            >
+              <Avatar alt={name} src={img} />
+            </StyledBadge>
+          ) : (
+            <Avatar alt={name} src={img} />
+          )}
+          <Stack spacing={0.3}>
+            <Typography variant="subtitle2">{name}</Typography>
+          </Stack>
+        </Stack>
+        <Stack direction={"row"} spacing={2} alignItems={"center"}>
+          <Button
+            onClick={() => {
+              socket.emit("friend_request", { to: _id, from: user_id }, () => {
+                alert("request sent");
+              });
+            }}
+          >
+            Send Request
+          </Button>
+        </Stack>
+      </Stack>
+    </StyledChatBox>
+  );
+    </>
   )
 }
 
